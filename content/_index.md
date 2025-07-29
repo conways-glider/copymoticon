0808824f28c630cc2cd1476c5d240b341f407499
Nia <nia@conwaysglider.com>
2025-07-28 19:56:29 -0600

    Switch to Astro

--- content/_index.md
+++
@@ -1,7 +0,0 @@
-+++
-title = "Home"
-# The index page will use the index.html template automatically.
-sort_by = "none"
-template = "index.html"
-paginate_by = 0
-+++
---
+++ src/data/emoticons.ts
@@ -0,0 +1,77 @@
+export const staticEmoticons: {category: string;name: string;symbol: string;}[] = [
+  {
+    category: "Happy",
+    name: "Smiley face",
+    symbol: ":)",
+  },
+  {
+    category: "Happy",
+    name: "Laughing",
+    symbol: "xD",
+  },
+  {
+    category: "Happy",
+    name: "Grinning",
+    symbol: "^_^",
+  },
+  {
+    category: "Happy",
+    name: "Winking",
+    symbol: ";)",
+  },
+  {
+    category: "Happy",
+    name: "Cheers",
+    symbol: "\\o/",
+  },
+  {
+    category: "Sad",
+    name: "Frowning",
+    symbol: ":(",
+  },
+  {
+    category: "Sad",
+    name: "Crying",
+    symbol: ":'(",
+  },
+  {
+    category: "Surprised",
+    name: "Shrug",
+    symbol: "¯\\_(ツ)_/¯",
+  },
+  {
+    category: "Angry",
+    name: "Angry",
+    symbol: "ಠ_ಠ",
+  },
+  {
+    category: "Cool",
+    name: "Lenny face",
+    symbol: "( ͡° ͜ʖ ͡°)",
+  },
+  {
+    category: "Confused",
+    name: "Confused",
+    symbol: "o_O",
+  },
+  {
+    category: "Love",
+    name: "Hug",
+    symbol: "(づ｡◕‿‿◕｡)づ",
+  },
+  {
+    category: "Misc",
+    name: "Table flip",
+    symbol: "(╯°□°）╯︵ ┻━┻",
+  },
+  {
+    category: "Misc",
+    name: "Put table back",
+    symbol: "┬─┬ ノ( ゜-゜ノ)",
+  },
+  {
+    category: "Misc",
+    name: "Cat smile",
+    symbol: "ฅ^•ﻌ•^ฅ",
+  },
+];
