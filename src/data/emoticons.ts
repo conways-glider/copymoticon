47fddfc239c4c92c2d4ceb9a2d11411831ec9507
Nia <nia@conwaysglider.com>
2026-04-22 19:11:41 -0600

    Organization

--- src/data/emoticons.ts
+++ src/data/emoticons.ts
@@ -5,6 +5,7 @@ export interface Emoticon {
 }
 
 export const StaticEmoticons: Emoticon[] = [
+  // Happy / joy
   {
     name: "Smile",
     symbol: ":)",
@@ -31,10 +32,37 @@ export const StaticEmoticons: Emoticon[] = [
     tags: ["happy", "joy"],
   },
   {
+    name: "Determined",
+    symbol: "(•̀ᴗ•́)و",
+    tags: ["happy", "cool"],
+  },
+  // Celebration
+  {
     name: "Cheers",
     symbol: "\\o/",
     tags: ["happy", "joy"],
   },
+  {
+    name: "Cheering",
+    symbol: "\\(^o^)/",
+    tags: ["happy", "celebration"],
+  },
+  {
+    name: "Hooray",
+    symbol: "ヽ(´▽`)/",
+    tags: ["happy", "celebration"],
+  },
+  {
+    name: "Party",
+    symbol: "ヽ(o＾▽＾o)ノ",
+    tags: ["happy", "celebration", "dance"],
+  },
+  {
+    name: "Excited",
+    symbol: "\\(★ω★)/",
+    tags: ["happy", "celebration"],
+  },
+  // Sad / disappointed
   {
     name: "Frowning",
     symbol: ":(",
@@ -51,54 +79,9 @@ export const StaticEmoticons: Emoticon[] = [
     tags: ["sad", "disappointed"],
   },
   {
-    name: "Shrug",
-    symbol: "¯\\_(ツ)_/¯",
-    tags: ["confused", "indifferent"],
-  },
-  {
-    name: "Angry",
-    symbol: "ಠ_ಠ",
-    tags: ["angry", "frustrated"],
-  },
-  {
-    name: "Lenny Face",
-    symbol: "( ͡° ͜ʖ ͡°)",
-    tags: ["cool", "mischievous"],
-  },
-  {
-    name: "Confused",
-    symbol: "o_O",
-    tags: ["confused", "indifferent"],
-  },
-  {
-    name: "Hug",
-    symbol: "(づ｡◕‿‿◕｡)づ",
-    tags: ["love", "affection"],
-  },
-  {
-    name: "Table Flip",
-    symbol: "(╯°□°）╯︵ ┻━┻",
-    tags: ["frustrated", "angry"],
-  },
-  {
-    name: "Put Table Back",
-    symbol: "┬─┬ ノ( ゜-゜ノ)",
-    tags: ["frustrated", "angry"],
-  },
-  {
-    name: "Double Table Flip",
-    symbol: "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻",
-    tags: ["frustrated", "angry"],
-  },
-  {
-    name: "Put Double Table Back",
-    symbol: "┬─┬ ︵ヽ(ಠ_ಠ)ﾉ︵ ┬─┬",
-    tags: ["frustrated", "angry"],
-  },
-  {
-    name: "Cat Smile",
-    symbol: "ฅ^•ﻌ•^ฅ",
-    tags: ["cute", "cat"],
+    name: "Disappointed Sigh",
+    symbol: "(ーー;)",
+    tags: ["sad", "disappointed"],
   },
   // Surprised / shocked
   {
@@ -121,6 +104,48 @@ export const StaticEmoticons: Emoticon[] = [
     symbol: "(⊙_⊙)",
     tags: ["surprised", "shocked"],
   },
+  // Angry / frustrated
+  {
+    name: "Angry",
+    symbol: "ಠ_ಠ",
+    tags: ["angry", "frustrated"],
+  },
+  {
+    name: "Table Flip",
+    symbol: "(╯°□°）╯︵ ┻━┻",
+    tags: ["frustrated", "angry"],
+  },
+  {
+    name: "Put Table Back",
+    symbol: "┬─┬ ノ( ゜-゜ノ)",
+    tags: ["frustrated", "angry"],
+  },
+  {
+    name: "Double Table Flip",
+    symbol: "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻",
+    tags: ["frustrated", "angry"],
+  },
+  {
+    name: "Put Double Table Back",
+    symbol: "┬─┬ ︵ヽ(ಠ_ಠ)ﾉ︵ ┬─┬",
+    tags: ["frustrated", "angry"],
+  },
+  // Confused / indifferent
+  {
+    name: "Shrug",
+    symbol: "¯\\_(ツ)_/¯",
+    tags: ["confused", "indifferent"],
+  },
+  {
+    name: "Confused",
+    symbol: "o_O",
+    tags: ["confused", "indifferent"],
+  },
+  {
+    name: "Embarrassed",
+    symbol: "(⌒_⌒;)",
+    tags: ["confused", "sad"],
+  },
   // Sleepy / tired
   {
     name: "Sleepy",
@@ -136,6 +161,53 @@ export const StaticEmoticons: Emoticon[] = [
     name: "Yawning",
     symbol: "(o´〰`o)",
     tags: ["sleepy", "tired"],
+  },
+  // Cool / mischievous
+  {
+    name: "Lenny Face",
+    symbol: "( ͡° ͜ʖ ͡°)",
+    tags: ["cool", "mischievous"],
+  },
+  {
+    name: "Cheeky",
+    symbol: "(•‿•)",
+    tags: ["mischievous", "happy"],
+  },
+  {
+    name: "Smug",
+    symbol: "(¬‿¬)",
+    tags: ["smug", "mischievous"],
+  },
+  {
+    name: "Side Eye",
+    symbol: "¬_¬",
+    tags: ["smug", "side-eye"],
+  },
+  {
+    name: "Skeptical",
+    symbol: "(￢_￢)",
+    tags: ["confused", "side-eye"],
+  },
+  // Love / affection
+  {
+    name: "Hug",
+    symbol: "(づ｡◕‿‿◕｡)づ",
+    tags: ["love", "affection"],
+  },
+  {
+    name: "Heart Eyes",
+    symbol: "(♥◡♥)",
+    tags: ["love", "affection"],
+  },
+  {
+    name: "Sending Love",
+    symbol: "(づ ◕‿◕ )づ ♡",
+    tags: ["love", "affection"],
+  },
+  {
+    name: "Heart",
+    symbol: "<3",
+    tags: ["love", "affection"],
   },
   // Greeting / wave
   {
@@ -153,7 +225,12 @@ export const StaticEmoticons: Emoticon[] = [
     symbol: "(￣^￣)ゞ",
     tags: ["greeting", "wave"],
   },
-  // Animals
+  // Cute / animals
+  {
+    name: "Cat Smile",
+    symbol: "ฅ^•ﻌ•^ฅ",
+    tags: ["cute", "cat"],
+  },
   {
     name: "Cat",
     symbol: "=^.^=",
@@ -194,26 +271,10 @@ export const StaticEmoticons: Emoticon[] = [
     symbol: "U・ﻌ・U",
     tags: ["cute", "animal", "dog"],
   },
-  // Celebration
   {
-    name: "Cheering",
-    symbol: "\\(^o^)/",
-    tags: ["happy", "celebration"],
-  },
-  {
-    name: "Hooray",
-    symbol: "ヽ(´▽`)/",
-    tags: ["happy", "celebration"],
-  },
-  {
-    name: "Party",
-    symbol: "ヽ(o＾▽＾o)ノ",
-    tags: ["happy", "celebration", "dance"],
-  },
-  {
-    name: "Excited",
-    symbol: "\\(★ω★)/",
-    tags: ["happy", "celebration"],
+    name: "Innocent",
+    symbol: "(◕‿◕✿)",
+    tags: ["cute", "happy"],
   },
   // Music / dance
   {
@@ -251,63 +312,5 @@ export const StaticEmoticons: Emoticon[] = [
     name: "Wink Sparkle",
     symbol: "(^_~)☆",
     tags: ["happy", "sparkle", "love"],
-  },
-  // More hearts / love
-  {
-    name: "Heart Eyes",
-    symbol: "(♥◡♥)",
-    tags: ["love", "affection"],
-  },
-  {
-    name: "Sending Love",
-    symbol: "(づ ◕‿◕ )づ ♡",
-    tags: ["love", "affection"],
-  },
-  {
-    name: "Heart",
-    symbol: "<3",
-    tags: ["love", "affection"],
-  },
-  // Smug / side-eye / mischief
-  {
-    name: "Smug",
-    symbol: "(¬‿¬)",
-    tags: ["smug", "mischievous"],
-  },
-  {
-    name: "Side Eye",
-    symbol: "¬_¬",
-    tags: ["smug", "side-eye"],
-  },
-  {
-    name: "Skeptical",
-    symbol: "(￢_￢)",
-    tags: ["confused", "side-eye"],
-  },
-  {
-    name: "Cheeky",
-    symbol: "(•‿•)",
-    tags: ["mischievous", "happy"],
-  },
-  // Bonus expressions
-  {
-    name: "Innocent",
-    symbol: "(◕‿◕✿)",
-    tags: ["cute", "happy"],
-  },
-  {
-    name: "Determined",
-    symbol: "(•̀ᴗ•́)و",
-    tags: ["happy", "cool"],
-  },
-  {
-    name: "Disappointed Sigh",
-    symbol: "(ーー;)",
-    tags: ["sad", "disappointed"],
-  },
-  {
-    name: "Embarrassed",
-    symbol: "(⌒_⌒;)",
-    tags: ["confused", "sad"],
   },
 ];
